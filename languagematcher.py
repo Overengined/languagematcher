b2ad0f40af2b8401597557e9a3d4a472524c984b
Hyperengined <71342876+Overengined@users.noreply.github.com>
2022-01-16 15:02:24 +0100

    Update languagematcher.py

--- languagematcher.py
+++ languagematcher.py
@@ -20,15 +20,21 @@ progressionhelper = 0
 progressionstatus = 0
 
 def progression():
+    '''
+    fonction gérant l'affichage de la progression
+    '''
     global text
     global progressionhelper
     global progressionstatus
     if progressionhelper >= len(text)/10 :
         progressionhelper = 0
-        print(round((progressionstatus/len(text))*100,0),"% done")
+        print(round((progressionstatus/len(text))*100,0),"% done", end="\r")
 
     
 def matchcount():
+    '''
+    fonctions permettant de dénombrer  
+    '''
     global matches
     global words
     global progressionhelper, progressionstatus
@@ -40,6 +46,8 @@ def matchcount():
         progression()
         progressionhelper += 1
         progressionstatus += 1
+
+
 
 #this piece of code allows the program to work in all possible directories, as lon a sys is present on the system
 #it creates a root path wich is the global path of the program's folder
@@ -57,12 +65,20 @@ for i in languages:
 print("reading text...")
 full_path = os.path.join(APP_FOLDER, "echantillon.txt")
 file = open(full_path)
-text = file.read().replace("\n", " ")
+text = file.read().replace("\n"," ")
 file.close()
+text = text.split(" ")
 
 print("counting matches...")
 matchcount()
 
+print("")
 print("en :",matches[0][1])
 print("fr :",matches[1][1])
+
+if matches[0][1] > matches[1][1] :
+    print("votre texte est écrit en Anglais, la langue de Shakespeare !")
+else:
+    print("votre texte est écrit en Francais, la langue de Molière !")
+
 input("")
